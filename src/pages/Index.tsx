cacb3b23acbedef088cdabb483ad50a87509ff86
Ahmed Gawish <ahmedgawish@Ahmeds-MacBook-Pro.local>
2025-03-07 18:45:11 -0500

    removed the scrolling helper effect

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,3 @@
-
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
 import { Services } from "@/components/Services";
@@ -6,15 +5,21 @@ import { ContactForm } from "@/components/ContactForm";
 
 const Index = () => {
   return (
-    <div className="h-screen snap-y snap-mandatory overflow-y-scroll">
+    <div className="h-screen overflow-y-scroll">
       <Navbar />
-      <div className="snap-start">
-        <Hero />
+
+      {/* Snap scrolling only between Hero and Services */}
+      <div className="h-screensnap-y snap-mandatory ">
+        <div className="snap-start h-screen">
+          <Hero />
+        </div>
+        <div className="snap-start h-screen">
+          <Services />
+        </div>
       </div>
-      <div className="snap-start">
-        <Services />
-      </div>
-      <div className="snap-start">
+
+      {/* Normal scrolling for ContactForm */}
+      <div className="mt-4 h-auto">
         <ContactForm />
       </div>
     </div>
