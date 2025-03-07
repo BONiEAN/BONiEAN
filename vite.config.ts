953cb2f1e95680329cfd7638be024da97ca9b56e
Ahmed Gawish <ahmedgawish@Ahmeds-MacBook-Pro.local>
2025-03-07 00:29:36 -0500

    adding base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/vite_react_shadcn_ts/",
+  base: mode === "development" ? "/" : "./",
   server: {
     host: "::",
     port: 8080,
